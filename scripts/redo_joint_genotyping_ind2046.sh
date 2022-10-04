e136637d5184daf49cc9fccb88e902c37ff5649f
ASendellPrice <ashley.sendell-price@zoo.ox.ac.uk>
2022-10-04 09:34:15 +0200

    redo ind2046

---
+++ scripts/redo_joint_genotyping_ind2046.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH -A snic2022-5-242
+#SBATCH -p core -N 1
+#SBATCH -t 3-00:00:00
+#SBATCH --array=1-10:1
+#SBATCH -J joint_genotyping
+
+#Load required modules
+module load bioinfo-tools GATK/4.2.0.0 bcftools
+
+#Set offspring ID
+OFFSPRING_ID=ind2046
+
+#Set block number
+BLOCK_NO=$SLURM_ARRAY_TASK_ID
+
+#Combine GVCFs
+gatk --java-options "-Xmx120g" CombineGVCFs \
+-R /proj/snic2020-2-19/private/shark/reference/satsuma/sHemOce1.mat.decon.20210528.fasta \
+--variant /proj/snic2020-2-19/private/shark/variants/gvcfs/${OFFSPRING_ID}_block_${BLOCK_NO}.g.vcf.gz \
+--variant /proj/snic2020-2-19/private/shark/variants/gvcfs/sHemOce2_block_${BLOCK_NO}.g.vcf.gz \
+--variant /proj/snic2020-2-19/private/shark/variants/gvcfs/sHemOce3_block_${BLOCK_NO}.g.vcf.gz \
+--convert-to-base-pair-resolution \
+-O block_${BLOCK_NO}.trio_${OFFSPRING_ID}.g.vcf.gz
+
+#Conduct joint genotyping using gVCFs
+gatk --java-options "-Xmx120g" GenotypeGVCFs \
+--include-non-variant-sites \
+-R /proj/snic2020-2-19/private/shark/reference/satsuma/sHemOce1.mat.decon.20210528.fasta \
+-V block_${BLOCK_NO}.trio_${OFFSPRING_ID}.g.vcf.gz \
+-O block_${BLOCK_NO}.trio_${OFFSPRING_ID}.vcf.gz
+
+#Remove gVCF and its index
+rm block_${BLOCK_NO}.trio_${OFFSPRING_ID}.g.vcf.gz*
